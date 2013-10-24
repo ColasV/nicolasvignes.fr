e50142ba86ea1ef2bf994bc34ac1c824aef376e7
Nicolas Vignes <nico.vignes@gmail.com>
2013-10-24 18:30:44 +0200

    Ajout des vues
    C'est mieux quand il y a les vues

---
+++ view/contact.php
@@ -0,0 +1,28 @@
+	
+	<div class="container">
+
+		
+
+	<div class="row-fluid">
+		<div class="span10 offset1">
+		<h2>CV</h2>		
+			<ul>
+				<li><a href="./contents/cv/cv-vignes-fr.pdf">CV en français</a></li>
+				<li><a href="./contents/cv/cv-vignes-en.pdf">CV in english</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="row-fluid">
+		<div class="span10 offset1">
+	<h2>Contact</h2>
+	<ul>
+		<li><a href="http://www.facebook.com/nico.vignes">Nicolas Vignes sur Facebook</a></li>
+		<li><a href="http://twitter.com/#!/nicolas_vignes">@nicolas_vignes sur Twitter</a></li>
+		<li><a href="https://plus.google.com/112175705138523847533/posts">Nicolas Vignes sur Google+</a></li>
+		<li><a href="http://fr.linkedin.com/pub/nicolas-vignes/58/68/b5/">Nicolas Vignes sur LinkedIn</a></li>
+	</ul>
+	</div>
+</div>
+</div>
+	
---
+++ view/publications.php
@@ -0,0 +1,49 @@
+
+	
+	<div class="container">
+
+					
+
+			<div class="row-fluid">
+				<div class="span10 offset1">
+	<h2>Informatiques</h2>
+	<h3>Site du Zéro</h3>
+	<p>J'ai publié actuellement deux cours sur le Site du Zéro ayant une thématique commune : le Mac. Mon premier tutoriel se propose d'introduire le logiciel de messagerie instantannée : Adium. Au travers de huit chapitres, l'utilisateur apprendra à se servir de cette application ainsi que d'utiliser et de créer des Xtras pour Adium, sorte de plug-in pour le logiciel.</p>
+	<ul>
+		<li><a href="http://www.siteduzero.com/tutoriel-3-320862-adium.html">Adium</a></li>
+	</ul>
+	<p>Le deuxième tutoriel porte sur une thématique beaucoup plus large : le système d'exploitation Mac OS X. Au travers de plus de 20 chapitres, l'utilisateur débutant apprendra à découvrir, utiliser et personnaliser son système d'exploitation selon ses goûts et besoins. L'apprentissage se veut progressif et tous les utilisateurs y trouveront leur compte pour apprendre à mieux gérer leur machine. Le tutoriel est à jour avec la dernière version de Mac OS X : Mountain Lion (10.8).</p>
+	<ul>
+		<li><a href="http://www.siteduzero.com/tutoriel-3-547902-debuter-avec-mac-os-x.html">Débuter avec Mac OS X</a></li>
+	</ul>
+
+	<h3>eBook</h3>
+	<p>Cet eBook est édité par Simple IT, l'entreprise qui gère le Site du Zéro, cet eBook reprend l'ensemble du tutoriel disponible sur le site au format epub sans DRM</p>
+	<ul>
+		<li><a href="http://boutique.siteduzero.com/boutique-614-1526-ebook-domptez-votre-mac-avec-mac-os-x-mountain-lion.html">Domptez votre Mac avec Mac OS X : Mountain Lion</a> (<a href="http://www.amazon.fr/Domptez-votre-avec-Mountain-ebook/dp/B00BG7JSKY/ref=sr_1_1?ie=UTF8&qid=1366668394&sr=8-1&keywords=domptez+votre+mac+avec+os+x">Amazon</a>, <a href="https://itunes.apple.com/fr/book/domptez-votre-mac-avec-mac/id603478071?mt=11">iTunes</a>)</li>
+	</ul>
+
+	<h3>Réseaux et télécommunications</h3>
+	<p>Voici une liste des articles que j'ai écris qui traitent de tout ce qui concerne les réseaux et les télécommunications.</p>
+	<ul>
+		<li><a href="./publications/difference_debit.pdf">Pourquoi le débit que me vend mon FAI n’est-il pas celui constaté ?</a></li>
+	</ul>
+
+	<h3>Wordpress</h3>
+	<p>Vous pourrez trouver mon premier thème pour Wordpress, qui utilise Bootstrap pour l'affichage</p>
+	<ul>
+		<li><a href="https://github.com/ColasV/blog_main">Dépôt GitHub du projet</a></li>
+	</ul>
+</div>
+</div>
+	<div class="row-fluid">
+				<div class="span10 offset1">
+	<h2>Scientifiques</h2>
+	<h3>TIPE</h3>
+	<p>Le TIPE (Travail d'initiative personnelle encadré) est une épreuve des concours aux grandes écoles. Vous pourrez trouver ici les slides de ma présentation sur le thème des Turbines Peltons, qui étudie leur fonctionnement avec quelques résultats théoriques classiques.</p>
+	<ul>
+		<li><a href="./publications/TIPE.pdf">TIPE : Turbine Pelton</a></li>
+	</ul>
+</div>
+</div>
+</div>
