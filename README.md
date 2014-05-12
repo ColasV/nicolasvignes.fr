98436bd8dabfe3ac53edd3c6061fc606dd9664ef
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2014-05-12 23:29:47 +0200

    New version

--- README.md
+++
@@ -1,5 +0,0 @@
-nicolasvignes.fr
-================
-
-Site Web Perso nicolasvignes.fr
-- Contenu pûrement statique pour le moment.
--- global/bas.php
+++
@@ -1,6 +0,0 @@
-<!-- jQuery -->
-<script src="//code.jquery.com/jquery.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="lib/bootstrap/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
